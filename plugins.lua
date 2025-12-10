666683439bd7baab8121fd77a24ae708c084d81c
TWoods <travis.woods2@outlook.com>
2025-12-10 23:20:13 +0800

    Update: Added run plugin compatibility for C/C++

--- plugins.lua
+++ plugins.lua
@@ -1,31 +1,54 @@
 -- Run current file in terminal window
 function Run_curr_file()
 	local file_name = vim.api.nvim_buf_get_name(0)
+	vim.cmd.write(file_name)
 
 	if string.sub(file_name, -3, -1) == ".py" then
 		local py_cmd = vim.api.nvim_replace_termcodes("i" .. 'python "' .. file_name .. '"<CR>', true, false, true)
-
-		-- Press keys to run python command on current file
-		vim.cmd.write(file_name)
 		vim.api.nvim_feedkeys(py_cmd, "t", false)
 	elseif string.sub(file_name, -3, -1) == ".js" then
 		local js_cmd = vim.api.nvim_replace_termcodes("i" .. 'node "' .. file_name .. '"<CR>', true, false, true)
-		vim.cmd.write(file_name)
 		vim.api.nvim_feedkeys(js_cmd, "t", false)
+	elseif string.sub(file_name, -2, -1) == ".c" then
+		-- local name = string.sub(vim.fn.expand("%:t"), 0, -3)
+		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -3) .. ".exe"
+		local c_cmd = vim.api.nvim_replace_termcodes(
+			"i" .. 'gcc "' .. file_name .. '" -o "' .. name .. '"<CR>',
+			true,
+			false,
+			true
+		)
+		local c_run = vim.api.nvim_replace_termcodes(name .. "<CR>", true, false, true)
+		vim.api.nvim_feedkeys(c_cmd, "t", false)
+		vim.api.nvim_feedkeys(c_run, "t", false)
+	elseif string.sub(file_name, -4, -1) == ".cpp" then
+		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -5) .. ".exe"
+		local cpp_cmd = vim.api.nvim_replace_termcodes(
+			"i" .. 'g++ "' .. file_name .. '" -o "' .. name .. '"<CR>',
+			true,
+			false,
+			true
+		)
+		local cpp_run = vim.api.nvim_replace_termcodes(name .. "<CR>", true, false, true)
+		vim.api.nvim_feedkeys(cpp_cmd, "t", false)
+		vim.api.nvim_feedkeys(cpp_run, "t", false)
 	else
+		local name = vim.fn.expand("%:t")
 		vim.print([[
-Cannot run this file; it is not supported by this plugin. --T
+Cannot run this file ']] .. name .. [['; it is not supported by this plugin. --T
 Supported file types include:
-	> Python
-	> JavaScript
-		]])
+    > Python
+    > JavaScript
+    > C
+    > C++
+        ]])
 		return 0
 	end
 
-	local percent_of_win = 0.25
+	local term_height = 0.25 -- Percentage of window height
 	local curr_win_height = vim.api.nvim_win_get_height(0)
-	local term_height = math.floor(curr_win_height * percent_of_win)
-	vim.cmd(":below " .. term_height .. "split | term")
+	local dis_height = math.floor(curr_win_height * term_height)
+	vim.cmd(":below " .. dis_height .. "split | term")
 end
 
 -- Highlight when yanking text
