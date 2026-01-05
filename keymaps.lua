c939b4f6a20dc3f0d5c777e840045b949c429b44
TWoods <travis.woods2@outlook.com>
2026-01-05 21:10:47 +0800

    Added Vse cmd to V-Split and Explore

--- keymaps.lua
+++ keymaps.lua
@@ -45,7 +45,7 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
---  See `:help wincmd` for a list of all window commands
+--  Window movement keybinds
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
@@ -62,3 +62,7 @@ vim.keymap.set("n", "<A-r>", Run_curr_file, { desc = "Run the current file in th
 
 -- neogit auto cwd=%
 vim.cmd([[noreabbrev <expr> ngit getcmdtype() == ':' && getcmdline() ==# 'ngit' ? 'Neogit cwd=%' : 'ngit']])
+
+-- vse to vs and Ex
+vim.api.nvim_create_user_command("Vse", "vsplit | Explore", { desc = "Vsplits and exits to Explorer" })
+vim.cmd([[cnoreabbrev <expr> vse getcmdtype() == ':' && getcmdline() ==# 'vse' ? 'Vse' : 'vse']])
