6ed77707b7bd06b06d8cd3cd815bb9c256aa739a
TWoods <travis.woods2@outlook.com>
2026-01-17 22:31:44 +0930

    Added stubs for proper c/c++ build.sh script support in <A-r> run

--- plugins.lua
+++ plugins.lua
@@ -11,44 +11,66 @@ function Run_curr_file()
 		vim.api.nvim_feedkeys(js_cmd, "t", false)
 	elseif string.sub(file_name, -2, -1) == ".c" then
 		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -3) .. ".exe"
-		local c_cmd = vim.api.nvim_replace_termcodes(
-			"i" .. 'gcc "' .. file_name .. '" -o "' .. name .. '"<CR>',
-			true,
-			false,
-			true
-		)
-		local c_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
-		vim.api.nvim_feedkeys(c_cmd, "t", false)
-		if "[no-error-from-terminal]" then
+		-- Check for build.sh or similar, run that if exists, else run standard "gcc main.c -o main"
+		if false then -- "[build.sh-exists]" then
+			local build_sh = "build.sh" -- change to found build.sh file
+			local build_sh_cmd =
+				vim.api.nvim_replace_termcodes("i" .. 'call "' .. build_sh .. '" <CR>', true, false, true)
+			vim.api.nvim_feedkeys(build_sh_cmd, "t", false)
+		else
+			local c_cmd = vim.api.nvim_replace_termcodes(
+				"i" .. 'gcc "' .. file_name .. '" -o "' .. name .. '"<CR>',
+				true,
+				false,
+				true
+			)
+			vim.api.nvim_feedkeys(c_cmd, "t", false)
+		end
+		if "[no-error-during-build]" then
+			local c_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
 			vim.api.nvim_feedkeys(c_run, "t", false)
 		end
 	elseif string.sub(file_name, -4, -1) == ".cpp" then
 		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -5) .. ".exe"
-		local cpp_cmd = vim.api.nvim_replace_termcodes(
-			"i" .. 'g++ "' .. file_name .. '" -o "' .. name .. '"<CR>',
-			true,
-			false,
-			true
-		)
-		local cpp_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
-		vim.api.nvim_feedkeys(cpp_cmd, "t", false)
-		if "[no-error-from-terminal]" then
+		-- Check for build.sh, run it if exists, else run standard "g++ main.cpp -o main"
+		if false then -- "[build.sh-exists]" then
+			local build_sh = "build.sh" -- change to found build.sh file
+			local build_sh_cmd = vim.api.nvim_replace_termcodes("i" .. '"' .. build_sh .. '" <CR>', true, false, true)
+		else
+			local cpp_cmd = vim.api.nvim_replace_termcodes(
+				"i" .. 'g++ "' .. file_name .. '" -o "' .. name .. '"<CR>',
+				true,
+				false,
+				true
+			)
+			vim.api.nvim_feedkeys(cpp_cmd, "t", false)
+		end
+		if "[no-error-during-build]" then
+			local cpp_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
 			vim.api.nvim_feedkeys(cpp_run, "t", false)
 		end
 	elseif string.sub(file_name, -4, -1) == ".ps1" then
 		local ps1_cmd =
 			vim.api.nvim_replace_termcodes("i" .. 'powershell "' .. file_name .. '" <CR>', true, false, true)
 		vim.api.nvim_feedkeys(ps1_cmd, "t", false)
+	elseif string.sub(file_name, -4, -1) == ".bat" then
+		local bash_cmd = vim.api.nvim_replace_termcodes("i" .. 'call "' .. file_name .. '" <CR>', true, false, true)
+		vim.api.nvim_feedkeys(bash_cmd, "t", false)
+	elseif string.sub(file_name, -3, -1) == ".sh" then
+		local shell_cmd = vim.api.nvim_replace_termcodes("i" .. 'call "' .. file_name .. '" <CR>', true, false, true)
+		vim.api.nvim_feedkeys(shell_cmd, "t", false)
 	else
 		local name = vim.fn.expand("%:t")
 		vim.print([[
 Cannot run this file ']] .. name .. [['; it is not supported by this plugin. --T
 Supported file types include:
-    > Python
-    > JavaScript
-    > C
-    > C++
-    > Powershell
+    > Python (.py)
+    > JavaScript (.js)
+    > C (.c) [option to use build.sh]
+    > C++ (.cpp) [option to use build.sh]
+    > Powershell (.ps1)
+    > batch (.bat)
+    > shell (.sh)
         ]])
 		return 0
 	end
