eab560275f46698d5fbc91f54282e1ee14f1473d
TWoods <travis.woods2@outlook.com>
2026-01-18 22:14:58 +0930

    Refactor to runfile to make it easier to read and update

--- runfile.lua
+++ runfile.lua
@@ -1,62 +1,72 @@
+local function run_cmd(cmd)
+	local command = vim.api.nvim_replace_termcodes("i" .. cmd .. "<CR>", true, false, true)
+	vim.api.nvim_feedkeys(command, "t", false)
+end
+
+local function get_os()
+	local os_name = vim.loop.os_uname().sysname
+	return os_name
+end
+
+local function get_call_func(os_name)
+	local call = ""
+	if os_name == "Windows_NT" then
+		call = "call "
+		-- elseif os_name == "<linux>" then
+		-- local call = "call"
+	else
+		vim.print("Unsupported OS '" .. os_name .. "'")
+	end
+	return call
+end
+
+local function has_suffix(str, substr)
+	local len = string.len(substr)
+	local contains = string.sub(str, -len, -1) == substr
+	return contains
+end
+
 function Run_curr_file()
 	local file_name = vim.api.nvim_buf_get_name(0)
+	vim.cmd.write(file_name)
 
-	if string.sub(file_name, -3, -1) == ".py" then
-		local py_cmd = vim.api.nvim_replace_termcodes("i" .. 'python "' .. file_name .. '"<CR>', true, false, true)
-		vim.api.nvim_feedkeys(py_cmd, "t", false)
-	elseif string.sub(file_name, -3, -1) == ".js" then
-		local js_cmd = vim.api.nvim_replace_termcodes("i" .. 'node "' .. file_name .. '"<CR>', true, false, true)
-		vim.api.nvim_feedkeys(js_cmd, "t", false)
-	elseif string.sub(file_name, -2, -1) == ".c" then
+	local os_name = get_os()
+	local call = get_call_func(os_name)
+
+	if has_suffix(file_name, ".py") then
+		run_cmd('python "' .. file_name .. '"')
+	elseif has_suffix(file_name, ".js") then
+		run_cmd('node "' .. file_name .. '"')
+	elseif has_suffix(file_name, ".c") then
 		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -3) .. ".exe"
 		-- Check for build.sh or similar, run that if exists, else run standard "gcc main.c -o main"
-		if false then -- "[build.sh-exists]" then
-			local build_sh = "build.sh" -- change to found build.sh file
-			local build_sh_cmd =
-				vim.api.nvim_replace_termcodes("i" .. 'call "' .. build_sh .. '" <CR>', true, false, true)
-			vim.api.nvim_feedkeys(build_sh_cmd, "t", false)
+		if false then -- "[build_file-exists]" then
+			local build_file = "build.sh" -- change to found build file
+			run_cmd(call .. '"' .. build_file .. '"')
 		else
-			local c_cmd = vim.api.nvim_replace_termcodes(
-				"i" .. 'gcc "' .. file_name .. '" -o "' .. name .. '"<CR>',
-				true,
-				false,
-				true
-			)
-			vim.api.nvim_feedkeys(c_cmd, "t", false)
+			run_cmd('gcc "' .. file_name .. '" -o "' .. name .. '"')
 		end
 		if "[no-error-during-build]" then
-			local c_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
-			vim.api.nvim_feedkeys(c_run, "t", false)
+			run_cmd('"' .. name .. '"')
 		end
-	elseif string.sub(file_name, -4, -1) == ".cpp" then
+	elseif has_suffix(file_name, ".cpp") then
 		local name = string.sub(vim.api.nvim_buf_get_name(0), 0, -5) .. ".exe"
 		-- Check for build.sh, run it if exists, else run standard "g++ main.cpp -o main"
-		if false then -- "[build.sh-exists]" then
-			local build_sh = "build.sh" -- change to found build.sh file
-			local build_sh_cmd = vim.api.nvim_replace_termcodes("i" .. '"' .. build_sh .. '" <CR>', true, false, true)
+		if false then -- "[build_file-exists]" then
+			local build_file = "build.sh" -- change to found build file
+			run_cmd(call .. '"' .. build_file .. '"')
 		else
-			local cpp_cmd = vim.api.nvim_replace_termcodes(
-				"i" .. 'g++ "' .. file_name .. '" -o "' .. name .. '"<CR>',
-				true,
-				false,
-				true
-			)
-			vim.api.nvim_feedkeys(cpp_cmd, "t", false)
+			run_cmd('g++ "' .. file_name .. '" -o "' .. name('"'))
 		end
 		if "[no-error-during-build]" then
-			local cpp_run = vim.api.nvim_replace_termcodes('"' .. name .. '" <CR>', true, false, true)
-			vim.api.nvim_feedkeys(cpp_run, "t", false)
+			run_cmd('"' .. name .. '"')
 		end
-	elseif string.sub(file_name, -4, -1) == ".ps1" then
-		local ps1_cmd =
-			vim.api.nvim_replace_termcodes("i" .. 'powershell "' .. file_name .. '" <CR>', true, false, true)
-		vim.api.nvim_feedkeys(ps1_cmd, "t", false)
-	elseif string.sub(file_name, -4, -1) == ".bat" then
-		local bash_cmd = vim.api.nvim_replace_termcodes("i" .. 'call "' .. file_name .. '" <CR>', true, false, true)
-		vim.api.nvim_feedkeys(bash_cmd, "t", false)
-	elseif string.sub(file_name, -3, -1) == ".sh" then
-		local shell_cmd = vim.api.nvim_replace_termcodes("i" .. 'call "' .. file_name .. '" <CR>', true, false, true)
-		vim.api.nvim_feedkeys(shell_cmd, "t", false)
+	elseif has_suffix(file_name, ".ps1") then
+		run_cmd('powershell "' .. file_name .. '"')
+	elseif has_suffix(file_name, ".bat") then
+		run_cmd(call .. '"' .. file_name .. '"')
+	elseif has_suffix(file_name, ".sh") then
+		run_cmd(call .. '"' .. file_name .. '"')
 	else
 		local name = vim.fn.expand("%:t")
 		vim.print([[
