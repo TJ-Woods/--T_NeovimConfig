8691c5a96dadf4fd741c1c8bf9fada1cf3a8213d
Tj-Woods <travis.woods2@outlook.com>
2026-04-19 20:33:06 +0930

    Add old shortcut commands to new file

---
+++ shortcuts.lua
@@ -0,0 +1,10 @@
+-- Tconfig
+tconfig = "~/.config/nvim/lua/--T/"
+vim.api.nvim_create_user_command("Tconfig", "Explore " .. tconfig, { desc = "Explores the nvim/lua/--T directory" })
+vim.cmd("cnoreabbrev <expr> tconfig getcmdtype() == ':' && getcmdline() ==# 'tconfig' ? 'Tconfig' : 'tconfig'")
+
+-- Pythondir
+pythondir = "~/Desktop/Coding/Python/"
+vim.api.nvim_create_user_command("Pythondir", "Explore " .. pythondir, { desc = "Explores the Coding/Python directory" })
+vim.cmd("cnoreabbrev <expr> pythondir getcmdtype() == ':' && getcmdline() ==# 'pythondir' ? 'Pythondir' : 'pythondir'")
+
