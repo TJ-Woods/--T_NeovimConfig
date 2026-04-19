350333488ed26fad0aa78e6daa7329ed0d376602
Tj-Woods <travis.woods2@outlook.com>
2026-04-19 20:29:34 +0930

    Plugin updates

--- plugins.lua
+++ plugins.lua
@@ -4,14 +4,82 @@ local gh = function(owner, name)
 end
 
 vim.pack.add({
-    gh("mofiqul", "vscode.nvim"),
-    gh("mbbill", "undotree"),
-    gh("nmac427", "guess-indent.nvim"),
-    gh("uga-rosa", "ccc.nvim"),
-    gh("linux-cultist", "venv-selector.nvim"),
-    gh("neogitorg", "neogit"),
-    gh("neovim", "nvim-lspconfig"),
-    gh("folke", "todo-comments.nvim")
+    gh("nvim-lua", "plenary.nvim"),             -- Dependancy for many plugins
+    gh("mofiqul", "vscode.nvim"),               -- Colour theme
+    gh("mbbill", "undotree"),                   -- Undotree
+    gh("nmac427", "guess-indent.nvim"),         -- Helps with autoindentation
+    gh("uga-rosa", "ccc.nvim"),                 -- Colour picker and display e.g. #000000 rgb(0, 0, 0) <-- This
+    gh("linux-cultist", "venv-selector.nvim"),  -- Allows python venv selection
+    gh("neogitorg", "neogit"),                  -- Git interface
+    gh("folke", "todo-comments.nvim"),          -- TODO: <-- this
+    gh("neovim", "nvim-lspconfig"),             -- LSP manager
+    gh("williamboman", "mason.nvim"),           --"
+    gh("williamboman", "mason-lspconfig.nvim"), --"
+    gh("tpope", "vim-abolish"),                 -- Case-Preserving text replacements
+    gh("nvim-mini", "mini.statusline"),         -- Nicer Status line
 })
 
+-- Start all loaded packages/plugins
+vim.cmd("packloadall")
+
+-- Colour scheme set to VSCode (Colour scheme)
 vim.cmd("colorscheme vscode")
+
+-- Guess Indent
+require("guess-indent").setup()
+
+-- Enable the following
+-- TODO: To Do - This is something that needs to be done
+--  Thing to do is also highlighted if next line is a comment
+-- NOTE: Note - this is a note
+-- OPTIMIZE: Performance - Optimise this
+-- TEST: Test - Test this section / this is a test
+-- HACK: Hacky code - This is weird
+-- WARN: Warning - This isn't working properly
+-- FIX:  Fix - This is broken. Please fix it
+require("todo-comments").setup()
+
+-- Enable venv-selector (python virtual environments)
+require("venv-selector").setup()
+
+-- Abolish (text capitalisation autocmds)
+-- require("vim-abolish").setup()
+
+-- Neogit
+require("neogit").setup()
+
+-- Mason.nvim Setup (LSP manager)
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls" },
+    handlers = {
+        function (server_name)
+            require("lspconfig")[server_name].setup({})
+        end,
+
+        ["lua_ls"] = function ()
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {'vim'},
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            })
+        end,
+    }
+})
