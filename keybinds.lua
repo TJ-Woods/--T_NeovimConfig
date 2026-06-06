abfd3d5a21509f8f2ae77971b988fce49be7d745
Tj-Woods <travis.woods2@outlook.com>
2026-06-06 21:22:35 +0930

    Added 'end' autocomplete for lua control structures

--- keybinds.lua
+++ keybinds.lua
@@ -228,6 +228,35 @@ vim.keymap.set("i", ")", function() return skip_closing(")") end, { desc = "Allo
 vim.keymap.set("i", "]", function() return skip_closing("]") end, { desc = "Allow Square Bracket Type-over", expr = true, silent = true })
 vim.keymap.set("i", "}", function() return skip_closing("}") end, { desc = "Allow Curly Bracket Type-over", expr = true, silent = true })
 
+
+-- Lua Specific
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "lua",
+    callback = function()
+        print("Running the Lua Return Autocmd")
+        -- Map <CR> in insert mode only for Lua files
+        vim.keymap.set("i", "<CR>", function()
+            local curs = vim.api.nvim_win_get_cursor(0)[2]
+            local line = vim.api.nvim_get_current_line()
+            local start_to_curs = string.sub(line, 1, curs)
+            local match_opener = start_to_curs:match("%s*function%s*[%w_.:]*%(.*%)%s*$")
+                or (start_to_curs:match("if%s+.*%s+then%s*$") and not start_to_curs:match(".*elseif.*"))
+                or start_to_curs:match("do%s*$")
+            local match_repeat = start_to_curs:match("repeat%s*$")
+
+            if match_opener then
+                print("LUA_ENTER")
+                return "<CR>end<Esc>O"
+            elseif match_repeat then
+                return "<CR>until "
+            else
+                return "<CR>"
+            end
+        end, { desc = "Auto-insert end for Lua blocks", expr = true, buffer = true })
+    end,
+})
+
+
 -- HTML Specific Keybinds
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "html",
@@ -272,7 +301,7 @@ vim.api.nvim_create_autocmd("FileType", {
                 return ">"
             end
 
-        end, { expr = true, silent = true, buffer = true, desc = "Smart HTML close tag" })
+        end, { desc = "Smart HTML close tag", expr = true, silent = true, buffer = true })
     end
 })
 
